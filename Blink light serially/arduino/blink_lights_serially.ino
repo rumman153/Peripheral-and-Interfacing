383a9bfbd7c4906f457abca840d2ce9baf98c1a2
Rumman Ahmed Prodhan <rumman153@gmail.com>
2021-01-17 18:32:42 +0600

    blink light serially

---
+++ Blink light serially/arduino/blink_lights_serially.ino
@@ -0,0 +1,49 @@
+void setup() {
+  pinMode (0,OUTPUT);
+  pinMode (1,OUTPUT);
+  pinMode (2,OUTPUT);
+  pinMode (3,OUTPUT);
+  pinMode (4,OUTPUT);
+  pinMode (5,OUTPUT);
+  pinMode (6,OUTPUT);
+  pinMode (7,OUTPUT);
+
+}
+
+
+
+void loop(){
+  digitalWrite (0,HIGH);
+  delay(1000);//milliseconds
+  digitalWrite(0,LOW);
+  delay(1000);
+  digitalWrite (1,HIGH);
+  delay(1100);//milliseconds
+  digitalWrite(1,LOW);
+  delay(1100);
+  digitalWrite (2,HIGH);
+  delay(1200);//milliseconds
+  digitalWrite(2,LOW);
+  delay(1200);
+  digitalWrite (3,HIGH);
+  delay(1300);//milliseconds
+  digitalWrite(3,LOW);
+  delay(1300);
+  digitalWrite (4,HIGH);
+  delay(1400);//milliseconds
+  digitalWrite(4,LOW);
+  delay(1400);
+  digitalWrite (5,HIGH);
+  delay(1500);//milliseconds
+  digitalWrite(5,LOW);
+  delay(1500);
+  digitalWrite (6,HIGH);
+  delay(1600);//milliseconds
+  digitalWrite(6,LOW);
+  delay(1600);
+  digitalWrite (7,HIGH);
+  delay(1700);//milliseconds
+  digitalWrite(7,LOW);
+  delay(1700);
+  
+}
